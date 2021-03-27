cdd6f09f638bc393bbee211ae8309c8d79b6b3a6
Michel Lamarlière <michel.lamarliere@icloud.com>
2021-03-28 00:38:43 +0100

    new design is almost done, need to prevent the user from submitting if the inputs are empty.

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ let title = document.getElementById('name');
 let author = document.getElementById('author');
 let pages = document.getElementById('pages');
 let read = document.getElementById('read-checkbox');
+let readOrNot = document.querySelector('#read-or-not');
 let submitBtn = document.getElementById('submit');
 
 // book card
 let cardContainer = document.querySelector('.card-container')
 let cards = document.getElementsByClassName('card');
+let cardDeleteBtns = document.getElementsByClassName('card-delete');
+let cardToggles = document.getElementsByClassName('card-toggle');
 
 // book stats
-let statsBooks = document.querySelector('.stats-books');
-let statsReadBooks = document.querySelector('.stats-books-read');
-let statsPages = document.querySelector('.stats-pages');
-let statsReadPages = document.querySelector('.stats-pages-read');
+let statsBooks = document.querySelector('#stats-books');
+let statsReadBooks = document.querySelector('#stats-books-read');
+let statsPages = document.querySelector('#stats-pages');
+let statsReadPages = document.querySelector('#stats-pages-read');
 
 //arrays
 let myLibrary = [];
@@ -45,12 +48,12 @@ function addBookToLibrary() {
 // display book card
 function displayBook() {
     function setAttribute() {
-        for (let y = 0; y < length; y++) {
-            cards[y].dataset.number = `${y}`;
-            cardDeleteBtn.dataset.number = `${y}`;
+        for (let i = 0; i < myLibrary.length; i++) {
+            cards[i].dataset.number = `${i}`;
+            cardDeleteBtns[i].dataset.number = `${i}`;
+            cardToggles[i].dataset.number = `${i}`;
         }
     }
-
     for (let i = myLibrary.length; i <= myLibrary.length; i++) {
         
         // creates Book Card
@@ -72,21 +75,32 @@ function displayBook() {
         // creates read/not read div
         let cardRead = document.createElement('div');
         cardRead.classList.add('read');
+        newBook.read == true ? cardRead.textContent = 'Read' : cardRead.textContent = 'Not Read';
+
+        let cardButtons = document.createElement('div');
+        cardButtons.classList.add('card-buttons');
+        
+        // creates read/not read button 
+        let cardToggle = document.createElement('button');
+        cardToggle.classList.add('card-toggle');
+        newBook.read == true ? cardToggle.textContent = 'Not Read Yet' : cardToggle.textContent = 'Read';
 
         // creates delete button div
         let cardDeleteBtn = document.createElement('button');
         cardDeleteBtn.classList.add('card-delete');
-        cardDeleteBtn.textContent = 'Delete';
+        cardDeleteBtn.textContent = '-';
 
         // appends divs to Book Card div
         card.appendChild(cardTitle);
         card.appendChild(cardAuthor);
         card.appendChild(cardPages);
         card.appendChild(cardRead);
-        card.appendChild(cardDeleteBtn);
+        card.appendChild(cardButtons);
+        cardButtons.appendChild(cardToggle);
+        cardButtons.appendChild(cardDeleteBtn);
         // appends Book Card to container
         cardContainer.appendChild(card);
-
+        setAttribute();
         // adds content
         cardTitle.textContent = 'Title: ' + newBook.title;
         cardAuthor.textContent = 'Author: ' + newBook.author;
@@ -95,15 +109,32 @@ function displayBook() {
         // adds read/not read
         newBook.read == true ? cardRead.textContent = 'Read' : cardRead.textContent = 'Not Read';
 
+        // toggle event listener
+        cardToggle.addEventListener('click', (event) => {
+            let number = event.target.dataset.number;
+            if (number && myLibrary[number].read === true) {
+                cardToggle.textContent = "Read";
+                myLibrary[number].read = false;
+                cardRead.textContent = 'Not Read'
+            } else {
+                cardToggle.textContent = "Not Read Yet";
+                myLibrary[number].read = true;
+                cardRead.textContent = 'Read';
+            }
+            displayStats();
+        })
+
         // delete event listener
         cardDeleteBtn.addEventListener('click', (event) => {
-            event.target.parentNode.remove();
+            event.target.parentNode.parentNode.remove();
             myLibrary.splice(event.target.dataset.number, 1);
-            let length = myLibrary.length;
             displayStats();
+            setAttribute();
         })  
     } 
+    
     displayStats();
+    
 }
 
 // display the stats
@@ -117,7 +148,7 @@ function displayStats() {
             readBooks++;
         }
     }
-    statsReadBooks.textContent = "ReadBooks: " + readBooks;
+    statsReadBooks.textContent = "Read Books: " + readBooks;
     // displays total pages
     let totalPages = 0;
     for (let i = 0; i < myLibrary.length; i++) {
@@ -132,8 +163,6 @@ function displayStats() {
         }
     }
     statsReadPages.textContent = "Read Pages: " + totalReadPages;
-
-    
 }
 
 // reset form
@@ -152,4 +181,12 @@ submitBtn.addEventListener('click', () => {
  // no letters in pages input
 pages.addEventListener('keyup', () => {
     pages.value = pages.value.replace(/[^\d]/,'');
+});
+
+read.addEventListener('click', () => {
+    if (read.checked) {
+        readOrNot.textContent = "Read";
+    } else {
+        readOrNot.textContent = "Not Read";
+    }
 })
