51492d1d098f277338c7e632890faec4ab24fab1
Michel Lamarlière <michel.lamarliere@icloud.com>
2021-03-28 20:58:50 +0200

    interface is done, next step is local storage

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // VARIABLES
 const main = document.getElementById('main');
 // form
-let title = document.getElementById('name');
+let title = document.getElementById('title');
 let author = document.getElementById('author');
 let pages = document.getElementById('pages');
-let read = document.getElementById('read-checkbox');
-let readOrNot = document.querySelector('#read-or-not');
+let read = document.querySelector('.form-checkbox');
+let cardReads = document.getElementsByClassName('read-or-not');
 let submitBtn = document.getElementById('submit');
 
 // book card
 let cardContainer = document.querySelector('.card-container')
 let cards = document.getElementsByClassName('card');
 let cardDeleteBtns = document.getElementsByClassName('card-delete');
-let cardToggles = document.getElementsByClassName('card-toggle');
+let cardToggles = document.getElementsByClassName('toggle-btn');
+let cardToggleBalls = document.getElementsByClassName('toggle-ball');
 
 // book stats
 let statsBooks = document.querySelector('#stats-books');
@@ -23,6 +24,9 @@ let statsReadPages = document.querySelector('#stats-pages-read');
 //arrays
 let myLibrary = [];
 let newBook;
+
+// emptyInputs() : checks if inputs are not empty
+let add;
 
 // values 
 let index;
@@ -38,11 +42,13 @@ function Book(title, author, pages, read) {
 
 // add book 
 function addBookToLibrary() {
-    newBook = new Book(title, author, pages, read)
-    index = myLibrary.length;
-    myLibrary.push(newBook);
-    displayBook();
-    resetForm();
+    if (add === true) {
+        newBook = new Book(title, author, pages, read)
+        index = myLibrary.length;
+        myLibrary.push(newBook);
+        displayBook();
+        resetForm();
+    } else return false;
 }
 
 // display book card
@@ -52,6 +58,7 @@ function displayBook() {
             cards[i].dataset.number = `${i}`;
             cardDeleteBtns[i].dataset.number = `${i}`;
             cardToggles[i].dataset.number = `${i}`;
+            cardToggleBalls[i].dataset.number = `${i}`;
         }
     }
     for (let i = myLibrary.length; i <= myLibrary.length; i++) {
@@ -59,6 +66,10 @@ function displayBook() {
         // creates Book Card
         let card = document.createElement('div');
         card.classList.add('card');
+
+        // creates fields parent div
+        let fieldsDiv = document.createElement('div');
+        fieldsDiv.classList.add('fields-div');
         
         // creates title div
         let cardTitle = document.createElement('div');
@@ -71,33 +82,65 @@ function displayBook() {
         // creates pages div
         let cardPages = document.createElement('div');
         cardPages.classList.add('pages');
-
-        // creates read/not read div
-        let cardRead = document.createElement('div');
-        cardRead.classList.add('read');
-        newBook.read == true ? cardRead.textContent = 'Read' : cardRead.textContent = 'Not Read';
-
+        
+        // creates toggle parent div
         let cardButtons = document.createElement('div');
         cardButtons.classList.add('card-buttons');
-        
-        // creates read/not read button 
-        let cardToggle = document.createElement('button');
-        cardToggle.classList.add('card-toggle');
-        newBook.read == true ? cardToggle.textContent = 'Not Read Yet' : cardToggle.textContent = 'Read';
 
+        // creates toggle and text parent div
+        let toggleDiv = document.createElement('div');
+        toggleDiv.classList.add('toggle-div');
+
+        // creates button parent div
+        let deleteDiv = document.createElement('div');
+        deleteDiv.classList.add('delete-div');
+
+        // creates read or not display 
+        let cardRead = document.createElement('div');
+        cardRead.classList.add('read-or-not');
+        if (newBook.read === true) {
+            cardRead.textContent = 'Read';
+        } else {
+            cardRead.textContent = 'Not Read';
+        }        
+        // creates read/not read button 
+        let cardToggle = document.createElement('div');
+        cardToggle.classList.add('toggle-btn');
+        if (newBook.read == true) {
+            cardRead.textContent = 'Read';
+            cardToggle.classList.add('toggle-on');
+        } else {
+            cardRead.textContent = 'Not Read';
+            cardToggle.classList.add('toggle-off');
+        }
+
+        // creates ball
+        let cardToggleBall = document.createElement('div');
+        cardToggleBall.classList.add('toggle-ball');
+        if (newBook.read === true) {
+            cardToggleBall.classList.add('toggle-on-ball');
+        } else {
+            cardToggleBall.classList.add('toggle-off-ball');
+        }
+        // appends ball to toggle
+        cardToggle.appendChild(cardToggleBall);
+        
         // creates delete button div
-        let cardDeleteBtn = document.createElement('button');
+        let cardDeleteBtn = document.createElement('div');
         cardDeleteBtn.classList.add('card-delete');
         cardDeleteBtn.textContent = '-';
 
         // appends divs to Book Card div
-        card.appendChild(cardTitle);
-        card.appendChild(cardAuthor);
-        card.appendChild(cardPages);
-        card.appendChild(cardRead);
+        card.appendChild(fieldsDiv)
+        fieldsDiv.appendChild(cardTitle);
+        fieldsDiv.appendChild(cardAuthor);
+        fieldsDiv.appendChild(cardPages);
         card.appendChild(cardButtons);
-        cardButtons.appendChild(cardToggle);
-        cardButtons.appendChild(cardDeleteBtn);
+        toggleDiv.appendChild(cardToggle);
+        toggleDiv.appendChild(cardRead)
+        deleteDiv.appendChild(cardDeleteBtn);
+        cardButtons.appendChild(toggleDiv);
+        cardButtons.appendChild(deleteDiv);
         // appends Book Card to container
         cardContainer.appendChild(card);
         setAttribute();
@@ -106,33 +149,37 @@ function displayBook() {
         cardAuthor.textContent = 'Author: ' + newBook.author;
         cardPages.textContent = 'Pages: ' + newBook.pages;
 
-        // adds read/not read
-        newBook.read == true ? cardRead.textContent = 'Read' : cardRead.textContent = 'Not Read';
-
         // toggle event listener
         cardToggle.addEventListener('click', (event) => {
             let number = event.target.dataset.number;
             if (number && myLibrary[number].read === true) {
-                cardToggle.textContent = "Read";
+                cardToggle.classList.remove('toggle-on');
+                cardToggle.classList.add('toggle-off');
+                cardToggleBall.classList.remove('toggle-on-ball');
+                cardToggleBall.classList.add('toggle-off-ball');
                 myLibrary[number].read = false;
-                cardRead.textContent = 'Not Read'
+                cardRead.textContent = 'Not Read';
             } else {
-                cardToggle.textContent = "Not Read Yet";
+                cardToggle.classList.remove('toggle-off');
+                cardToggle.classList.add('toggle-on');
+                cardToggleBall.classList.remove('toggle-off-ball');
+                cardToggleBall.classList.add('toggle-on-ball');
                 myLibrary[number].read = true;
                 cardRead.textContent = 'Read';
             }
             displayStats();
+            setAttribute();
         })
 
         // delete event listener
         cardDeleteBtn.addEventListener('click', (event) => {
-            event.target.parentNode.parentNode.remove();
+            event.target.parentNode.parentNode.parentNode.remove();
             myLibrary.splice(event.target.dataset.number, 1);
             displayStats();
             setAttribute();
         })  
     } 
-    
+    setAttribute();
     displayStats();
     
 }
@@ -172,9 +219,33 @@ function resetForm() {
     pages.value = "";
 }
 
+function emptyInputs() {
+    add = true;
+    if (title.value == "") {
+        add = false;
+        title.placeholder = 'Please enter a title';
+    }
+    if (author.value == "") {
+        add = false;
+        author.placeholder = 'Please enter an author';
+    }
+    if (pages.value == "") {
+        add = false;
+        pages.placeholder = 'Please enter a page number';
+    }
+
+    if (add === true) {
+        title.placeholder = 'Title';
+        author.placeholder = 'Author';
+        pages.placeholder = 'Pages';
+    }
+    return add;
+}
+
 // EVENT LISTENERS
 // submit button
 submitBtn.addEventListener('click', () => {
+    emptyInputs();
     addBookToLibrary();
 });
 
@@ -183,10 +254,8 @@ pages.addEventListener('keyup', () => {
     pages.value = pages.value.replace(/[^\d]/,'');
 });
 
-read.addEventListener('click', () => {
-    if (read.checked) {
-        readOrNot.textContent = "Read";
-    } else {
-        readOrNot.textContent = "Not Read";
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        submitBtn.click();
     }
 })
